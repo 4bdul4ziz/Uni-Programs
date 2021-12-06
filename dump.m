8e89b3625363c501fceae8efef04f75f72cef937
Abdul Aziz <66479632+4bdul4ziz@users.noreply.github.com>
2021-12-06 21:52:57 +0530

    matlab cgs

--- dump.m
+++ dump.m
@@ -1,13 +1,20 @@
-clear all
 clc
-l1=input('Enter Value for Link 1: ');
-l2=input('Enter Value for Link 2: ');
-l3=input('Enter Value for Link 3: ');
-for i=1:360;
-    w(i)=((l2*l3)*(sqrt((l2^2)+(l3^2)+(2*l2*l3*cosd(i))))*abs(sind(i)));
-end 
-plot(w);
-theta3=acosd((sqrt(((l2^2)+(l3^2))^2+(12*(l2^2)*(l3^2)))-((l2^2)+(l3^2)))/(6*l2*l3))
-W=((l2*l3)*(sqrt((l2^2)+(l3^2)+(2*l2^2*l3^2*cosd(theta3))))*abs(sind(theta3)));
-fprintf('Value of Theta3: %d',theta3);
-fprintf('\nValue of W: %d\n',W);
+clear all
+l1=input('enter value for link1: ');
+m1=input('enter mass1: ');
+v1=input('enter v1: ');
+a1=input('enter acc1: ');
+g=9.8;
+l2=input('enter value for link2: ');
+m2=input('enter mass2: ');
+a2=input('enter acc2: '); 
+v2=input('enter v2: ');
+theta1=input('enter theta1: ');
+theta2= -90:1:90;
+t1=(m1*l1^2 + m2*(l1^2 + 2*l1*l2*cosd(theta2) + l2^2))*a1 + m2*(l1*l2*cosd(theta2) + l2^2)*a2 - m2*l1*l2*sind(theta2)*(2*v1*v2 + v2^2) + (m1 + m2)*l1*9.8*cosd(theta1) + m2*9.8*l2*cosd(theta1+theta2);
+t2=m2*(l1*l2*cosd(theta2)+l2^2)*a1+m2*l2^2*a2+m2*l1*l2*v1^2*sind(theta2)+m2*9.8*l2*cosd(theta1+theta2);
+plot(theta2,t2);
+hold on;
+plot(theta2,t1);
+xlabel('Theta2');
+ylabel('Torque');
