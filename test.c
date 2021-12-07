d51498acd68412a525520e91857efb083da2a0a0
Abdul Aziz <66479632+4bdul4ziz@users.noreply.github.com>
2021-12-08 01:45:40 +0530

    child process testing

--- test.c
+++ test.c
@@ -3,65 +3,17 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-/*
-Rule 1: no process can print other process's id
-Rule 2: no parent process id is printed before its Child
-*/
 
 int main()
 {
-   pid_t pid1, pid2, pid3, pid4, pid5, pid6;
-
-   printf("The parent (of all) process is A : %d", getpid()); //main parent process
-   pid1 = fork();
-
-   if (pid1 < 0)
+   for (int i = 0; i < 1; i++)
    {
-      printf("Fork failed ");
-   }
-
-   else if (pid1 == 0)
-   { //process B - child 1
-      printf("Child 1 with id: %d and its Parent id: %d \n", getpid(), getppid());
-      pid2 = fork();
-      if (pid2 == 0)
-      { //process E child 1.1
-         printf("Child 1.1 with id: %d and its Parent id: %d \n", getpid(), getppid());
-      }
-      else
+      if (fork() == 0)
       {
-         pid3 = fork();
-         if (pid3 == 0)
-         { //process F child 1.2
-            printf("Child 1.2 with id: %d and its Parent id: %d \n", getpid(), getppid());
-         }
-         else
-         {
-            pid4 = fork();
-            if (pid4 == 0)
-            { //process G child 1.3
-               printf("Child 1.3 with id: %d and its Parent id: %d \n", getpid(), getppid());
-            }
-         }
+         printf("[son] pid %d from [parent] pid %d\n", getpid(), getppid());
+         exit(0);
       }
    }
-
-   else
-   {
-      pid5 = fork();
-      if (pid5 == 0)
-      { //process C child 2
-         printf("Child 2 with id: %d and its Parent id: %d \n", getpid(), getppid());
-         pid6 = fork();
-         if (pid6 == 0)
-         { //process D child 2.1
-            printf("Child 2.1 with id: %d and its Parent id: %d \n", getpid(), getppid());
-         }
-      }
-   }
-   for (int i = 0; i < 3; i++)
-   {
+   for (int i = 0; i < 1; i++)
       wait(NULL);
-   }
-   return 0;
 }
