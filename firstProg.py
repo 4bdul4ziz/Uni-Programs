370870cf31252209ac64c894d15671311271e8fc
Abdul Aziz <66479632+4bdul4ziz@users.noreply.github.com>
2021-12-10 12:48:56 +0530

    list input to set output

--- firstProg.py
+++ firstProg.py
@@ -1,1 +1,12 @@
-print("Due to unfortunate circumstances, I'm alive.")
+xa = []
+y = int(input())
+for x in range(0, y):
+    element = int(input())
+    xa.append(element)
+z = set()
+result = []
+for x in xa:
+    if x not in z:
+        result.append(x)
+        z.update(xa)
+print(z)
