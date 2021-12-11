16d8d91252db4e13d65c53d09222d2547247b067
Abdul Aziz <66479632+4bdul4ziz@users.noreply.github.com>
2021-12-11 10:06:48 +0530

    matlab

--- dump.m
+++ dump.m
@@ -1,20 +1,26 @@
-clc
 clear all
-l1=input('enter value for link1: ');
-m1=input('enter mass1: ');
-v1=input('enter v1: ');
-a1=input('enter acc1: ');
-g=9.8;
-l2=input('enter value for link2: ');
-m2=input('enter mass2: ');
-a2=input('enter acc2: '); 
-v2=input('enter v2: ');
-theta1=input('enter theta1: ');
-theta2= -90:1:90;
-t1=(m1*l1^2 + m2*(l1^2 + 2*l1*l2*cosd(theta2) + l2^2))*a1 + m2*(l1*l2*cosd(theta2) + l2^2)*a2 - m2*l1*l2*sind(theta2)*(2*v1*v2 + v2^2) + (m1 + m2)*l1*9.8*cosd(theta1) + m2*9.8*l2*cosd(theta1+theta2);
-t2=m2*(l1*l2*cosd(theta2)+l2^2)*a1+m2*l2^2*a2+m2*l1*l2*v1^2*sind(theta2)+m2*9.8*l2*cosd(theta1+theta2);
-plot(theta2,t2);
-hold on;
-plot(theta2,t1);
-xlabel('Theta2');
-ylabel('Torque');
+clc
+max_voltage=240;
+min_voltage=210;
+a=120;
+b=320;
+voltage=(b-a)*rand(100,1)+a;
+new_voltage=[];
+for i=0:99
+	if((voltage(i+1)>max_volatge) &&(voltage(i+1)>min_voltage))
+		new_voltage(i+1)=max_voltage;
+	elseif((voltage(i+1)<min_voltage)&&(voltage(i+1)<max_voltage))
+		new_voltage(i+1)=min_voltage;
+	else
+		new_voltage(i+1)=voltage(i+1);
+	end
+end
+x=linspace(0,99,100);
+plot(x,voltage,'DisplayName','Unregulated voltage');
+title('Unregulated vs Regulated voltage');
+xlabel('Time (in seconds)')
+ylabel('Voltage (in V) ')
+hold on
+plot(x,new_voltage,'DisplayName','Unregulated voltage');
+hold off
+legend
