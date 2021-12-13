69d57819e4a8d8c60ca90299a50a4c77191f1d25
Abdul Aziz <66479632+4bdul4ziz@users.noreply.github.com>
2021-12-14 00:41:52 +0530

    ennumeration

--- crashPy.py
+++ crashPy.py
@@ -1,1 +1,12 @@
-print(((10 >= 5*2) and (10 <= 5*2)))
+import time
+start = time.time()
+total, s = 100000, 0
+a = [True] * total
+a[0] = a[1] = False
+for i, isprime in enumerate(a):
+    if isprime:
+        s += 1
+        for n in range(i*i, total, i):
+            a[n] = False
+print(time.time() - start)
+print(s)
