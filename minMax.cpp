dcce67e7009e99454473a1adf28cc96e0d2585e2
Abdul Aziz <66479632+4bdul4ziz@users.noreply.github.com>
2022-01-24 00:33:46 +0530

    DAA lab 2

--- minMax.cpp
+++ minMax.cpp
@@ -1,25 +1,73 @@
-auto it = max_element(std::begin(cloud), std::end(cloud)); // c++11
-
-template <typename T, size_t N>
-const T *mybegin(const T (&a)[N]) { return a; }
-template <typename T, size_t N>
-const T *myend(const T (&a)[N]) { return a + N; }
-
 #include <iostream>
+#include <chrono>
 #include <algorithm>
-
-template <typename T, size_t N>
-const T *mybegin(const T (&a)[N])
-{
-    return a;
-}
-template <typename T, size_t N>
-const T *myend(const T (&a)[N]) { return a + N; }
+using namespace std;
+void insertionSort(int arr[], int length);
+void printArray(int array[], int size);
 
 int main()
 {
-    const int cloud[] = {1, 2, 3, 4, -7, 999, 5, 6};
+    using namespace std::chrono;
+    auto start = high_resolution_clock::now();
+    int p = 10;
+    int i, j;
+    int arr[10] = {89, 79, 97, 69, 56, 89, 99, 100, 78, 67};
+    insertionSort(arr, p);
 
-    std::cout << *std::max_element(mybegin(cloud), myend(cloud)) << '\n';
-    std::cout << *std::min_element(mybegin(cloud), myend(cloud)) << '\n';
+    cout << "\n Max Marks = " << *max_element(arr, arr + p) << endl;
+    cout << "\n Min Marks = " << *min_element(arr, arr + p) << endl;
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    int x = 10;
+    int y = duration.count();
+    FILE *fp = NULL;
+    FILE *gnupipe = NULL;
+    char *GnuCommands[] = {"set title \"Abdul Aziz A.B - 20BRS1185 - Insertion Sort Graph\"", "plot 'data.tmp'"};
+
+    fp = fopen("data.tmp", "w");
+    gnupipe = popen("gnuplot -persitent", "w");
+
+    for (int i = 0; i < x; i++)
+    {
+        fprintf(fp, "%d %d\n", x, y);
+
+        x = x - 1;
+        y = y - 1;
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        fprintf(gnupipe, "%s\n", GnuCommands[i]);
+    }
+
+    cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
+    return 0;
+}
+
+void insertionSort(int arr[], int length)
+{
+    int i, j, tmp;
+    for (i = 1; i < length; i++)
+    {
+        j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            tmp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = tmp;
+            j--;
+        }
+        printArray(arr, 10);
+    }
+}
+void printArray(int array[], int size)
+{
+    cout << "Sorting the array using insertion";
+    int j;
+    for (j = 0; j < size; j++)
+        for (j = 0; j < size; j++)
+            cout << " " << array[j];
+    cout << endl;
 }
