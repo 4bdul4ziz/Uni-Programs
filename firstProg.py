4603f6506d3c41a78ccf75629af2651609770d91
Abdul Aziz <66479632+4bdul4ziz@users.noreply.github.com>
2022-02-03 17:22:03 +0530

    numpy trials

--- firstProg.py
+++ firstProg.py
@@ -1,12 +1,9 @@
-xa = []
-y = int(input())
-for x in range(0, y):
-    element = int(input())
-    xa.append(element)
-z = set()
-result = []
-for x in xa:
-    if x not in z:
-        result.append(x)
-        z.update(xa)
-print(z)
+# plotting a curve import matplotlib.pyplot as pltimport numpy as np
+import matplotlib as plt
+import numpy as np
+x = np.linspace(0, 20, 200)
+y = 2*np.sin(x)
+fig, ax = plt.subplots()
+ax.plot(x, y, linewidth=2.0, color='r')
+ax.set(xlim=(0, 20), ylim=(-3, 3))
+plt.show()
