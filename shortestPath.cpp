dda1d8b9bda6914c3f3a88701a6b561c5dbd9cea
Abdul Aziz <66479632+4bdul4ziz@users.noreply.github.com>
2022-03-03 14:37:40 +0530

    daa lab 4a

--- shortestPath.cpp
+++ shortestPath.cpp
@@ -1,52 +1,76 @@
-// implement djikstra's shortest path algorithm
-//
-//
-
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstdlib>
+#include <limits.h>
 #include <chrono>
 
-void djikstra()
+using namespace std;
+
+#define V 9
+
+int minDistance(int dist[], bool sptSet[])
 {
-    // read in graph
-    int n, m;
-    std::cin >> n >> m;
-    std::vector<std::vector<int>> graph(n, std::vector<int>(n, INT_MAX));
-    for (int i = 0; i < m; i++)
+    int min = INT_MAX, min_index;
+    for (int v = 0; v < V; v++)
     {
-        int u, v, w;
-        std::cin >> u >> v >> w;
-        graph[u][v] = w;
-        graph[v][u] = w;
+        if (sptSet[v] == false && dist[v] <= min)
+        {
+            min = dist[v], min_index = v;
+        }
     }
-
-    // read in start and end
-    int start, end;
-    std::cin >> start >> end;
-
-    // initialize distance
-    std::vector<int> distance(n, INT_MAX);
-    distance[start] = 0;
-    std::vector<bool> visited(n, false);
+    return min_index;
 }
 
+void printSolution(int dist[])
+{
+    cout << "Vertex \t Distance from Source" << endl;
+    for (int i = 0; i < V; i++)
+    {
+        cout << i << " \t\t" << dist[i] << endl;
+    }
+}
+
+void dijkstra(int graph[V][V], int src)
+{
+    int dist[V];
+    bool sptSet[V];
+    for (int i = 0; i < V; i++)
+    {
+        dist[i] = INT_MAX, sptSet[i] = false;
+    }
+    dist[src] = 0;
+    for (int count = 0; count < V - 1; count++)
+    {
+        int u = minDistance(dist, sptSet);
+        sptSet[u] = true;
+        for (int v = 0; v < V; v++)
+        {
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
+            {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+    printSolution(dist);
+}
 
 int main()
 {
     using namespace std::chrono;
     auto start = high_resolution_clock::now();
-    int graph[][3] = {{0, 1, 2}, {0, 2, 3}, {0, 3, 4}, {1, 2, 5}, {1, 3, 6}, {1, 4, 7}, {2, 4, 8}, {3, 4, 9}, {3, 5, 10}, {4, 5, 11}, {4, 6, 12}};
-    djikstra(graph, 9);
-
+    int graph[V][V] = {
+        {0, 4, 0, 0, 0, 0, 0, 8, 0},
+        {4, 0, 8, 0, 0, 0, 0, 11, 0},
+        {0, 8, 0, 7, 0, 4, 0, 0, 2},
+        {0, 0, 7, 0, 9, 14, 0, 0, 0},
+        {0, 0, 0, 9, 0, 10, 0, 0, 0},
+        {0, 0, 4, 14, 10, 0, 2, 0, 0},
+        {0, 0, 0, 0, 0, 2, 0, 1, 6},
+        {8, 11, 0, 0, 0, 0, 1, 0, 7},
+        {0, 0, 2, 0, 0, 0, 6, 7, 0}};
+    dijkstra(graph, 0);
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
 
-    int x = 10;
+    int x = 9;
     int y = duration.count();
     FILE *fp = NULL;
     FILE *gnupipe = NULL;
@@ -68,7 +92,6 @@ int main()
         fprintf(gnupipe, "%s\n", GnuCommands[i]);
     }
 
-    std::cout << "Time taken by function: "
-              << duration.count() << " microseconds" << std::endl;
+    cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
     return 0;
 }
